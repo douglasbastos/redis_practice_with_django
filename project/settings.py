d1719bb9e9cc277c79e525d7648b84cf17845e40
Hudson Conceição Da Silva <hud90cs@gmail.com>
2015-09-04 19:37:44 -0300

    mudar nome do banco

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'simpleblog',
+        'NAME': 'livramento',
         'USER': 'root',
         'PASSWORD': '',
     }

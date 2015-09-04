f58f5158164c5a650ad89e9ecc11513ae90cc989
Hudson Conceição Da Silva <hud90cs@gmail.com>
2015-09-04 19:56:52 -0300

    Ajustando urls não encontradas e retirando a app do blog

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'acrux_blog',
     'ranking_real_time'
 )
 
--- project/urls.py
+++ project/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('acrux_blog.urls')),
-    url(r'^', include('acruxtable.urls')),
-    url(r'^ranking', include('ranking_real_time.urls')),
 ]

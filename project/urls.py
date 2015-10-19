94f1e41fdd28fb0f39b6cdee6fc3f6ed49b2dc47
Douglas Bastos <douglashsb@gmail.com>
2015-10-19 19:38:09 -0200

    Ajusta urls

--- project/urls.py
+++ project/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^ranking/', include('ranking_real_time.urls')),
-    url(r'^blog/', include('acrux_blog.urls')),
-    url(r'^grappelli/', include('grappelli.urls')),
+    url('^admin/', include(admin.site.urls)),
+    url('^ranking/', include('ranking_real_time.urls')),
+    url('^blog/', include('acrux_blog.urls')),
+    url('^grappelli/', include('grappelli.urls')),
 ]

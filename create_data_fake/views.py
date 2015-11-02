75cedfed771e1877d972cf09d8746542a57fea50
Douglas Bastos <douglashsb@gmail.com>
2015-11-02 20:36:30 -0200

    Adiciona opção de inserir espaço

--- create_data_fake/views.py
+++ create_data_fake/views.py
@@ -16,11 +16,13 @@ An habeo populo scripta eos, ad invidunt recusabo consequuntur mei. Vidit etiam 
 '''
 
 
-def generator_string_by_range(type_string='letters', min=6, max=12):
+def generator_string_by_range(type_string='letters', min=6, max=12, space=False):
     max_length = choice(range(min, max+1))
+    # aumenta as chances de vir um vázio colocando 4x
+    spa = '    ' if space else ''
     if type_string == 'lowercase':
-        return str(''.join(choice(string.ascii_lowercase) for i in range(max_length)))
+        return str(''.join(choice(string.ascii_lowercase+spa) for i in range(max_length)))
     elif type_string == 'uppercase':
-        return str(''.join(choice(string.ascii_uppercase) for i in range(max_length)))
+        return str(''.join(choice(string.ascii_uppercase+spa) for i in range(max_length)))
     else:
-        return str(''.join(choice(string.ascii_letters) for i in range(max_length)))
+        return str(''.join(choice(string.ascii_letters+spa) for i in range(max_length)))

eeaed733387fa7025bbcc3e4aa6268885837f45c
Douglas Bastos <douglashsb@gmail.com>
2015-11-10 23:36:23 -0200

    Adiciona memcached

---
+++ project/settings_memcached.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from settings import *
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': TIME_CACHE,
+    }
+}
